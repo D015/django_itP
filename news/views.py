6a6743ceaf12cd910b1fc45b403a913c686289e5
Dmitry Zagorevskiy <7281015@gmail.com>
2020-12-21 00:27:36 +0100

    lesson 10

--- news/views.py
+++ news/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
+from django.views.generic import DetailView
 
 
 def news_home(request):
     # news = Article.objects.all().order_by('-date')[:0]
     news = Article.objects.all().order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def create(request):
