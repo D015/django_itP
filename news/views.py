38de3565b205b97abc111cdadfc080639de8a271
Dmitry Zagorevskiy <7281015@gmail.com>
2020-12-21 12:40:53 +0100

    lesson 11

--- news/views.py
+++ news/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView, DeleteView
 
 
 def news_home(request):
     # news = Article.objects.all().order_by('-date')[:0]
     news = Article.objects.all().order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
-
-
-class NewsDetailView(DetailView):
-    model = Article
-    template_name = 'news/details_view.html'
-    context_object_name = 'article'
 
 
 def create(request):
@@ -32,3 +26,22 @@ def create(request):
         'error': error,
     }
     return render(request, 'news/create.html', data)
+
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Article
+    template_name = 'news/create.html'
+
+    form_class = ArticleForm
+
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    success_url = '/news/'
+    template_name = 'news/news-delete.html'
